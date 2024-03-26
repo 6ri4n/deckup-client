b27b51090a611831c368b7b157332778a949354a
Liam DeWitt <liamdewitt96@gmail.com>
2024-03-26 14:52:03 -0400

    change file pathing and add front end routing in app.jsx. Temporary NotFound page created.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import LandingPage from "./pages/LandingPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/signup",
+    element: <SignupPage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/introduction",
+    element: <LandingPage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: "/",
+    element: <div>Home Page</div>,
+    errorElement: <NotFoundPage />,
+  },
+]);
+
 function App() {
-<<<<<<< HEAD
-  return (
-    <div>
-      <ToastNotif />
-    </div>
-  );
-=======
-  return <div>Test</div>;
->>>>>>> 9f9e1a4440d98c2dbb1e8af45fb436796490eb30
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/LandingPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function LandingPage() {
-  return <div>Landingpage</div>;
-}
-
-export default LandingPage;
