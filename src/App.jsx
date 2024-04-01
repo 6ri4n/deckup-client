9308163a22972bd7688b9659a9810fccc53f5950
brian <bri4n.huang@gmail.com>
2024-03-31 22:36:42 -0700

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
 function App() {
-<<<<<<< HEAD
-  return (
-    <div>
-      <ToastNotif />
-    </div>
-  );
-=======
-  return <div>Test</div>;
->>>>>>> 9f9e1a4440d98c2dbb1e8af45fb436796490eb30
+  return <div>Deck Up</div>;
 }
 
 export default App;
