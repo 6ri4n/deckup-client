9723355d40f2e24ff03d8f55aec0182583f2eb22
Liam DeWitt <liamdewitt96@gmail.com>
2024-05-03 01:35:33 -0400

    add deck view component

---
+++ src/components/Deck/Deck.jsx
@@ -0,0 +1,26 @@
+const Deck = () => {
+  return (
+    <div className="flex flex-col border border-red-600 bg-blue-950 gap-3">
+      <div className="border border-red-600 bg-gray-700 mt-3">
+        Deck 1<div>deck title</div>
+      </div>
+      <div className="border border-red-600 bg-gray-700">
+        Deck 2<div>deck title</div>
+      </div>
+      <div className="border border-red-600 bg-gray-700">
+        Deck 3<div>deck title</div>
+      </div>
+      <div className="border border-red-600 bg-gray-700">
+        Deck 4<div>deck title</div>
+      </div>
+      <div className="border border-red-600 bg-gray-700">
+        Deck 5<div>deck title</div>
+      </div>
+      <div className="border border-red-600 bg-gray-700 mb-3">
+        Deck 6<div>deck title</div>
+      </div>
+    </div>
+  );
+};
+
+export default Deck;
