650e84fa2a2169923a4a72058b18335a5b588453
brian <bri4n.huang@gmail.com>
2024-05-08 19:08:19 -0700

    add user data to be stored in cookie

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -3,15 +3,45 @@ import { createContext, useContext, useEffect, useState } from "react";
 const AuthContext = createContext(null);
 
 export default function AuthProvider({ children }) {
-  const [user, setUser] = useState({ isSignedIn: false });
+  const storedUser = getCookie("user");
+
+  const [user, setUser] = useState(
+    storedUser ? JSON.parse(storedUser) : { isSignedIn: false }
+  );
+
+  useEffect(() => {
+    setCookie("user", JSON.stringify(user));
+  }, [user]);
 
   const login = (userData) => {
     setUser((prev) => ({ ...prev, ...userData, isSignedIn: true }));
   };
 
   const logout = () => {
+    deleteCookie("user");
     setUser({ isSignedIn: false });
   };
+
+  function getCookie(name) {
+    const cookies = document.cookie.split("; ");
+
+    for (let cookie of cookies) {
+      const [cookieName, cookieValue] = cookie.split("=");
+      if (cookieName === name) {
+        return decodeURIComponent(cookieValue);
+      }
+    }
+
+    return null;
+  }
+
+  function setCookie(name, value) {
+    document.cookie = `${name}=${encodeURIComponent(value)};`;
+  }
+
+  function deleteCookie(name) {
+    document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+  }
 
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
