f943c533b9c7249456e6c13e7d1d93754a08ca1b
brian <bri4n.huang@gmail.com>
2024-05-18 20:42:12 -0700

    update navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,9 +1,26 @@
 import { useState } from "react";
 import MobileButton from "../ui/MobileButton";
-function Navbar({ priNav1, priNav2, secNav1, secNav2 }) {
+import { useAuth } from "../AuthProvider";
+import useApi from "../../hooks/useApi";
+import { useNavigate } from "react-router-dom";
+
+function Navbar() {
+  const { logout } = useAuth();
+
+  const { sendRequest } = useApi();
+
+  const navigate = useNavigate();
+
   const [expand, setExpand] = useState(false);
+
   const handleDropDown = () => {
     setExpand(!expand);
+  };
+
+  const handleLogout = async () => {
+    await sendRequest("POST", "/api/account/logout");
+    logout();
+    navigate("/");
   };
 
   return (
@@ -13,14 +30,17 @@ function Navbar({ priNav1, priNav2, secNav1, secNav2 }) {
           <div className="flex space-x-4 ">
             {/* primary nav */}
             <div className="hidden md:flex flex items-center space-x-8 px-10">
-              <a href="#" className="hover:text-red-600">
-                {priNav1 ? priNav1 : "Home"}
+              <a href="/home" className="hover:text-red-600">
+                Home
               </a>
-              <a href="#" className="hover:text-red-600">
-                {priNav2 ? priNav2 : "Play"}
+              <a href="/create-deck" className="hover:text-red-600">
+                Create
               </a>
-              <a href="#" className="hover:text-red-600">
-                {secNav1 ? secNav1 : "Logout"}
+              <a
+                className="hover:text-red-600 hover:cursor-pointer"
+                onClick={handleLogout}
+              >
+                Logout
               </a>
             </div>
           </div>
@@ -31,22 +51,22 @@ function Navbar({ priNav1, priNav2, secNav1, secNav2 }) {
         {expand && (
           <div className="md:hidden">
             <a
-              href="#"
+              href="/home"
               className="block py-2 px-4 text-sm hover:bg-gray-200 hover:text-red-600"
             >
-              {priNav1 ? priNav1 : "Home"}
+              Home
             </a>
             <a
-              href="#"
+              href="/create-deck"
               className="block py-2 px-4 text-sm hover:bg-gray-200 hover:text-red-600"
             >
-              {priNav2 ? priNav2 : "Play"}
+              Create
             </a>
             <a
-              href="#"
-              className="block py-2 px-4 text-sm hover:bg-gray-200 hover:text-red-600"
+              className="block py-2 px-4 text-sm hover:bg-gray-200 hover:text-red-600 hover:cursor-pointer"
+              onClick={handleLogout}
             >
-              {secNav1 ? secNav1 : "Logout"}
+              Logout
             </a>
           </div>
         )}
